2f3f68e4d23d416a4cd303878589427fea33971b
MakzZi <makzzi777@gmail.com>
2018-04-23 02:21:47 +0300

    Created a new class ReadInputConsole with the getInt() and getIntArray() methods. Adding to the Main.java method readConsoleIntArray()

--- src/com/makzzi/Main.java
+++ src/com/makzzi/Main.java
@@ -2,7 +2,19 @@ package com.makzzi;
 
 public class Main {
 
+    private int[] readConsoleIntArray() {
+        int length = 0;
+        ReadInputConsole ric = new ReadInputConsole();
+        while (length < 1) {
+            System.out.printf("Enter a positive length for the array: ");
+            length = ric.getInt();
+        }
+        System.out.printf("Enter an integer for the array: ");
+        return ric.getIntArray(length);
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        Main main = new Main();
+        main.readConsoleIntArray();
     }
 }
---
+++ src/com/makzzi/ReadInputConsole.java
@@ -0,0 +1,30 @@
+package com.makzzi;
+
+import java.util.Scanner;
+
+public class ReadInputConsole {
+
+    public int[] getIntArray(int lengthArray) {
+        try {
+            int[] arr = new int[lengthArray];
+            for (int i = 0; i < lengthArray; i++)
+                arr[i] = getInt();
+            return arr;
+        }
+        catch (NegativeArraySizeException ex) {
+            System.out.printf("Exception: %s%nEnter a positive length for the array: ", ex);
+            int index = getInt();
+            while (index < 0) index = getInt();
+            return getIntArray(index);
+        }
+    }
+
+    public int getInt() {
+        Scanner scan = new Scanner(System.in);
+        if (scan.hasNextInt()) {
+            int i = scan.nextInt();
+            return i;
+        }
+        return getInt();
+    }
+}
