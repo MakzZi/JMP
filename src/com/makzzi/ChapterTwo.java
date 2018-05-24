ae711b2329e15b8b9b03b003e246a0c7ba7b8b02
Maxim Zlodeev <makzzi777@gmail.com>
2018-05-24 14:19:07 +0300

    Completed the task #2.4.b

--- src/com/makzzi/ChapterTwo.java
+++ src/com/makzzi/ChapterTwo.java
@@ -302,4 +302,23 @@ public class ChapterTwo {
             System.out.printf("x1 = %.3f%nx2 = %.3f%n", (-b + d) / (2*a), (-b - d) / (2*a));
     }
 
+    public String month(int num) {
+        switch (num) {
+            case 1: return "January";
+            case 2: return "February";
+            case 3: return "March";
+            case 4: return "April";
+            case 5: return "May";
+            case 6: return "June";
+            case 7: return "July";
+            case 8: return "August";
+            case 9: return "September";
+            case 10: return "October";
+            case 11: return "November";
+            case 12: return "December";
+            default: return "Invalid month";
+        }
+    }
+
+
 }
